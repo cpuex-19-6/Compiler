08ab37b5aeb5e7432ec3a68401db8769041dc825
7ma7X <hellorusk1998@gmail.com>
2019-11-05 15:43:33 +0900

    テストを更新

--- test/float-check.ml
+++ test/float-check.ml
@@ -1,58 +1,12 @@
-let rec test_fadd m n =
-  m +. n in
-let rec test_fsub m n =
-  m -. n in
-let rec test_fmul m n =
-  m *. n in
-let rec test_fdiv m n =
-  m /. n in
-let rec test_sqrt n =
-  sqrt n in
-let rec test_sin n =
-  sin n in
-let rec test_cos n =
-  cos n in
-let rec test_atan n =
-  atan n in
-let rec test_fhalf n = (* ×0.5 *)
-  fhalf n in
-let rec test_fsqr n = (* n×n *)
-  fsqr n in
-let rec test_fabs n =
-  fabs n in
-let rec test_fneg n =
-  fneg n in
-let rec test_fless m n = (* m < n *)
-  fless m n in
-let rec test_fiszero n = 
-  fiszero n in
-let rec test_fispos n =
-  fispos n in
-let rec test_fisneg n =
-  fisneg n in
-let rec test_floor n = (* 床関数 *)
-  floor n in
-let rec test_ftoi n = (* int_of_float *)
-  int_of_float n in
-let rec test_itof n = (* float_of_int *)
-  float_of_int n in
-let _ = test_fadd 0.2 0.5 in
-let _ = test_fsub 0.2 0.5 in
-let _ = test_fmul 0.2 0.5 in
-let _ = test_fdiv 0.2 0.5 in
-let _ = test_sqrt 1.0 in
-let _ = test_sin 1.0 in
-let _ = test_cos 1.0 in
-let _ = test_atan 1.0 in
-let _ = test_fhalf 1.0 in
-let _ = test_fsqr 1.0 in
-let _ = test_fabs (-1.0) in
-let _ = test_fneg (-1.0) in
-let _ = test_fless 0.2 0.5 in
-let _ = test_fiszero 0.1 in
-let _ = test_fispos 0.1 in
-let _ = test_fisneg 0.1 in
-let _ = test_floor (-0.25) in
-let _ = test_ftoi 3.0 in
-let _ = test_itof 3 in
-()
+print_int (int_of_float (3.5 +. 2.5));
+print_int (int_of_float (3.5 -. 2.5));
+print_int (int_of_float (2.5 *. 0.4));
+print_int (int_of_float (1.0 /. 0.2));
+print_int (int_of_float (sqrt 4.0));
+print_int (int_of_float (sin 0.0));
+print_int (int_of_float (cos 0.0));
+print_int (int_of_float (atan 0.0));
+print_int (int_of_float (fhalf 4.0));
+print_int (int_of_float (fsqr 2.0));
+print_int (int_of_float (fabs (-2.0)));
+print_int (int_of_float (float_of_int 5));
